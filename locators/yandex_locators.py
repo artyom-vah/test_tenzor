3426b05fd8346a9f7a620186a3c90997843334ff
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-06-26 22:13:01 +0500

    доработал тест 2го сценария без PageObject

--- locators/yandex_locators.py
+++ locators/yandex_locators.py
@@ -9,10 +9,4 @@ class SearchPageLocators:
 
 
 class ImagesPageLocators:
-    SEARCH_INPUT = (By.CSS_SELECTOR, "input[name='text']")
-    SERVICES_MENU = (By.CSS_SELECTOR, ".services-suggest__item-more")
-    IMAGES_LINK = (By.CSS_SELECTOR, "div.services-more-popup__section-content:nth-child(1) > span:nth-child(9) > a:nth-child(2)")
-    FIRST_CATEGORY = (By.CLASS_NAME, "PopularRequestList-Item_pos_0")
-    FIRST_IMAGE = (By.CSS_SELECTOR, ".serp-item__link")
-
-
+    pass
--- tests/tests_vresion_1_correct_del.py
+++ tests/tests_vresion_1_correct_del.py
@@ -16,30 +16,30 @@ def browser():
 
 
 def test_search_yandex(browser):
-    # Шаг 1: Зайти на https://ya.ru/
+    # 1: Зайти на https://ya.ru/
     browser.get("https://ya.ru/")
 
-    # Шаг 2: Проверить наличие поля поиска
+    # 2: Проверить наличие поля поиска
     search_input = browser.find_element(By.CSS_SELECTOR, "input[name='text']")
     assert search_input.is_displayed()
 
-    # Шаг 3: Ввести в поиск "Тензор"
+    # 3: Ввести в поиск "Тензор"
     search_input.send_keys("Тензор")
 
-    # Шаг 4: Проверить, что появилась таблица с подсказками (suggest)
+    # 4: Проверить, что появилась таблица с подсказками (suggest)
     suggest_table = browser.find_element(By.CLASS_NAME, "mini-suggest__overlay_visible")
     assert suggest_table.is_displayed()
 
-    # Шаг 5: Нажать Enter
+    # 5: Нажать Enter
     search_input.send_keys(Keys.ENTER)
 
-    # Шаг 6: Проверить, что отображаются результаты поиска
+    # 6: Проверить, что отображаются результаты поиска
     search_results = WebDriverWait(browser, 5).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, '.serp-item.serp-item_card'))
     )
     assert search_results.is_displayed()
 
-    # Шаг 7: Проверить, что первая ссылка ведет на сайт tensor.ru
+    # 7: Проверить, что первая ссылка ведет на сайт tensor.ru
     first_link = search_results.find_element(By.TAG_NAME, 'a')
     href = first_link.get_attribute("href")
     assert "tensor.ru" in href
@@ -47,45 +47,64 @@ def test_search_yandex(browser):
 
 def test_yandex_image(browser):
     # 1. Зайти на ya.ru
-    browser.get("https://ya.ru")
+    browser.get("https://ya.ru/")
 
-    # 2. Нажать на поиск
+    # 1.1. Нажать на поиск
     search_input = browser.find_element(By.CSS_SELECTOR, "input[name='text']")
     search_input.click()
 
-    # 3 открыть меню 'все сервисы'
-    services_menu = browser.find_element(By.CSS_SELECTOR, ".services-suggest__item-more")
-    assert services_menu.is_displayed(), "кнопка меню 'все сервисы' не присутствует на странице"
-    services_menu.click()
+    # 2. Проверить, что кнопка меню(сервисы) присутствует на странице
+    menu_button = browser.find_element(By.CSS_SELECTOR, '.services-suggest__list-item-more')
+    assert menu_button.is_displayed(), "Кнопка меню-сервисы не присутствует на странице"
+    menu_button.click()
 
-    # 4 выбрать "картинки"
-    images_link = browser.find_element(By.CSS_SELECTOR, "div.services-more-popup__section-content:nth-child(1) > span:nth-child(9) > a:nth-child(2)")
+    # 3. Выбрать “Картинки”
+    images_link = browser.find_element(By.XPATH, '//a[@href="https://yandex.ru/images/"]')
     images_link.click()
 
-    browser.get("https://yandex.ru/images/")
+    # Переключиться на новую вкладку
+    browser.switch_to.window(browser.window_handles[-1])
 
-    # 5. Проверить, что перешли на url https://yandex.ru/images/
-    assert browser.current_url == "https://yandex.ru/images/", "Вы не перешли по адресу https://yandex.ru/images/"
+    # Дождаться, пока URL станет ожидаемым
+    expected_url = "https://yandex.ru/images/"
+    WebDriverWait(browser, 10).until(EC.url_to_be(expected_url))
 
-    # 6. Открыть первую категорию
+    # 4. Проверить, что перешли на URL https://yandex.ru/images/
+    actual_url = browser.current_url
+    assert actual_url == expected_url, f"Ожидался URL: {expected_url}, получен URL: {actual_url}"
+
+    # 5. Открыть первую категорию
     first_category = browser.find_element(By.CLASS_NAME, "PopularRequestList-Item_pos_0")
     first_category.click()
+
+    # 6. Проверить, что название категории отображается в поле поиска
+    search_input = browser.find_element(By.CSS_SELECTOR, "input[name='text']")
+    assert search_input.get_attribute("value"), "Название категории не отображается в поле поиска"
 
     # 7. Открыть 1 картинку
     first_image = browser.find_element(By.CSS_SELECTOR, ".serp-item__link")
     first_image.click()
 
     # 8. Проверить, что картинка открылась
-    assert "https://yandex.ru/images/" in browser.current_url, "Картинка не открылась"
+    image_8th = browser.find_element(By.CSS_SELECTOR, '.Link_view_default')
+    assert image_8th.is_displayed(), "Картинка не открылась"
+
+    # получаем название картинки на 8м шаге, чтоб сравнить с шагом 10 и 12
+    image_8th_text = image_8th.text
+    print(image_8th_text)
 
     # 9. Нажать кнопку вперед
-    browser.back()
+    button_next = browser.find_element(By.CSS_SELECTOR, 'div.CircleButton_type_next')
+    button_next.click()
 
-    # 10. Проверить, что картинка сменилась
-    assert "https://yandex.ru/images/" in browser.current_url
+    # 10. Проверить, что картинка сменилась, сравниваем текст картинки 8 и 10
+    image_10th = browser.find_element(By.CSS_SELECTOR, '.Link_view_default')
+    assert image_10th.text != image_8th_text, "Картинка осталась без изменений"
 
     # 11. Нажать назад
-    browser.forward()
+    button_back = browser.find_element(By.CSS_SELECTOR, 'div.CircleButton_type_prev')
+    button_back.click()
 
     # 12. Проверить, что картинка осталась из шага 8
-    assert "https://yandex.ru/images/" in browser.current_url
+    image_12th = browser.find_element(By.CSS_SELECTOR, '.Link_view_default')
+    assert image_12th.text == image_8th_text, "Картинка изменилась"
